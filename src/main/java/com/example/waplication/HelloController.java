f4fefc2c92acc8faa4798fc12ee71955b8ad0964
RdsJava <RdsJava@yandex.ru>
2024-05-02 19:25:25 +0300

    Windows Application JAVAFX 8 len v1

--- src/main/java/com/example/waplication/HelloController.java
+++ src/main/java/com/example/waplication/HelloController.java
@@ -12,9 +12,6 @@ import java.io.*;
 public class HelloController {
 
     @FXML
-    private boolean checkButton;
-
-    @FXML
     private CheckBox checkBoxOpHl;
     @FXML
     private CheckBox checkBoxCiklo;
@@ -31,83 +28,80 @@ public class HelloController {
     @FXML
     private CheckBox checkBoxRozOp;
 
-    @FXML
-    private Button buttonCreation;
 
     @FXML
-    private TextArea text;
+    private TextArea texts;
 
     @FXML
-    void buttonClick(ActionEvent event) {
-        if (!buttonCreation.isDefaultButton()) {
-            checkButton = true;
-            System.out.println("Кнопка нажата");
-        }
-    }
+    private String text;
+
 
     @FXML
     void checkBoxInitialize(ActionEvent actionEvent) {
+        String d = texts.getText();
 
-            TextAudioOporiHladovit textToAudioOporiHladovit = new TextAudioOporiHladovit();
-            TextToAudioCiklo textToAudioCiklo = new TextToAudioCiklo();
-            TextToAudioNomernoi textToAudioNomernoi = new TextToAudioNomernoi();
-            TextToAudioRitmo textToAudioRitmo = new TextToAudioRitmo();
-            TextToAudioOporiRadasteid textToAudioOporiRadasteid = new TextToAudioOporiRadasteid();
-            TextToAudioR100 textToAudioR100 = new TextToAudioR100();
-            TextToAudioRadasteid textToAudioRadasteid = new TextToAudioRadasteid();
+        //Запись в файл текста:
+        try {
+            FileWriter writer = new FileWriter("F:\\textToAudio/doc.txt");
+            writer.write(d);
+            writer.flush();//Данные, которые вы записываете в Writer, иногда временно хранятся в буфере, метод flush() используется для сброса (flush) всех данных из буфера в целевой объект.
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        BufferedReader brTest = null;
+        try {
+            brTest = new BufferedReader(new FileReader("F:\\textToAudio/doc.txt"));
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
 
-            String d = text.getText();
-
-            //Запись в файл текста:
-            try {
-                FileWriter writer = new FileWriter("F:\\textToAudio/doc.txt");
-                writer.write(d);
-                writer.flush();//Данные, которые вы записываете в Writer, иногда временно хранятся в буфере, метод flush() используется для сброса (flush) всех данных из буфера в целевой объект.
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-            BufferedReader brTest = null;
-            try {
-                brTest = new BufferedReader(new FileReader("F:\\textToAudio/doc.txt"));
-            } catch (FileNotFoundException ex) {
-                throw new RuntimeException(ex);
-            }
-
-            String text = null;
-            try {
-                text = brTest.readLine();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-            try {
-                if (checkBoxOpHl.isSelected()) {
-                    textToAudioOporiHladovit.textToAudioOporiHladovit(text);
-                }
-                if (checkBoxCiklo.isSelected()) {
-                    textToAudioCiklo.textToAudioCiklo(text);
-                }
-                if (checkBoxNomernoi.isSelected()) {
-                    textToAudioNomernoi.textToAudioNomernoi(text);
-                }
-                if (checkBoxRitmo.isSelected()) {
-                    textToAudioRitmo.textToAudioRitmo(text);
-                }
-                if (checkBoxOpRd.isSelected()) {
-                    textToAudioOporiRadasteid.textToAudioOporiRadasteid(text);
-                }
-                if (checkBoxOpRd.isSelected()) {
-                    textToAudioR100.textToAudioR100(text);
-                }
-                if (checkBoxRd.isSelected()) {
-                    textToAudioRadasteid.textToAudioRadasteid(text);
-                }
-                if (checkBoxRozOp.isSelected()) {
-                    textToAudioRadasteid.textToAudioRadasteid(text);
-                }
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+        try {
+            text = brTest.readLine();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
+    @FXML
+    void buttonClick(ActionEvent event) {
+
+        TextAudioOporiHladovit textToAudioOporiHladovit = new TextAudioOporiHladovit();
+        TextToAudioCiklo textToAudioCiklo = new TextToAudioCiklo();
+        TextToAudioNomernoi textToAudioNomernoi = new TextToAudioNomernoi();
+        TextToAudioRitmo textToAudioRitmo = new TextToAudioRitmo();
+        TextToAudioOporiRadasteid textToAudioOporiRadasteid = new TextToAudioOporiRadasteid();
+        TextToAudioR100 textToAudioR100 = new TextToAudioR100();
+        TextToAudioRadasteid textToAudioRadasteid = new TextToAudioRadasteid();
+
+        try {
+
+            if (checkBoxOpHl.isSelected()) {
+                textToAudioOporiHladovit.textToAudioOporiHladovit(text);
+            }
+            if (checkBoxCiklo.isSelected()) {
+                textToAudioCiklo.textToAudioCiklo(text);
+            }
+            if (checkBoxNomernoi.isSelected()) {
+                textToAudioNomernoi.textToAudioNomernoi(text);
+            }
+            if (checkBoxRitmo.isSelected()) {
+                textToAudioRitmo.textToAudioRitmo(text);
+            }
+            if (checkBoxOpRd.isSelected()) {
+                textToAudioOporiRadasteid.textToAudioOporiRadasteid(text);
+            }
+            if (checkBoxR100.isSelected()) {
+                textToAudioR100.textToAudioR100(text);
+            }
+            if (checkBoxRd.isSelected()) {
+                textToAudioRadasteid.textToAudioRadasteid(text);
+            }
+            if (checkBoxRozOp.isSelected()) {
+                textToAudioRadasteid.textToAudioRadasteid(text);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
 
 
